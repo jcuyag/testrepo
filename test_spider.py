abd7ec6fec668f3765128e068b0fbde15e6b5371
Jesse Cuyag <jc@Jesses-MacBook-Pro.local>
2015-03-10 17:53:35 +0800

    my first file

---
+++ test_spider.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestSpider(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+
+	def test_method(self):
+		print "this is testing only"
+
+if __name__ == '__main__':
+	unittest.main()
