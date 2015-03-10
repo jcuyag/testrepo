b940266606818014b3914838970620ef6b1f6d68
jcuyag <jkuyag@gmail.com>
2015-03-10 20:56:29 +0800

    test commit

--- test_spider.py
+++ test_spider.py
@@ -18,7 +18,7 @@ class TestSpider(unittest.TestCase):
 		self.assertEqual(2, 2)
 
 	def test_method_three(self):
-		self.assertNotEqual(2, 5)
+		self.assertNotEqual(2, 2)
 
 if __name__ == '__main__':
 	unittest.main()
